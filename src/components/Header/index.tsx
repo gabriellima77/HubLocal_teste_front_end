3a8247b6cb097561bc06626a990b25fbc0390e78
Gabriel Lima <gabrielpk1@live.com>
2022-04-17 14:04:23 -0300

    add eslint configuration

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,16 +1,21 @@
-import styles from './Header.module.scss';
+import { useContext } from "react";
 
+import { AuthContext } from "../../contexts/AuthContext";
+import styles from "./Header.module.scss";
 
 interface HeaderProps {
   name: string;
 }
 
 export function Header({ name }: HeaderProps) {
+  const { signOut } = useContext(AuthContext);
   return (
     <header className={styles.header}>
       <div>Logo</div>
       <p>{name}</p>
-      <button>Logout</button>
+      <button type="button" onClick={() => signOut()}>
+        Logout
+      </button>
     </header>
   );
 }
